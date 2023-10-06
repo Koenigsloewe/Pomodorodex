d193a89d3af0f0a2d770e9f021ffc9b2b79207f8
Koenigsloewe <ThithuSiva@protonmail.com>
2023-10-06 17:40:31 +0200

    Update pomodoro_timer.py

--- app/components/pomodoro_timer.py
+++ app/components/pomodoro_timer.py
@@ -1,5 +1,5 @@
-from PyQt5.QtCore import Qt, pyqtSignal, QTime, QTimer
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt, pyqtSignal, QTime, QTimer, QSize
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QLabel, QPushButton
 
 try:
@@ -7,7 +7,10 @@ try:
 except ModuleNotFoundError:
     from .progress_bar_timer import ProgressBar
 
-# QWidget[class="apps widget"] whats that  kind or reference?
+try:
+    from app.resources import resources
+except ModuleNotFoundError:
+    from app.resources import resources
 
 
 class Timer(QWidget):
@@ -78,23 +81,31 @@ class Timer(QWidget):
         self.grouped_btn_layout.setSpacing(0)
 
         # content of group btn (start, pause and stop btn)
-        self.start_btn = QPushButton("Start")
+        self.start_btn = QPushButton("")
+        self.start_btn.setIcon(QIcon(QPixmap(":/svg/play-fill.svg")))
+        self.start_btn.setIconSize(QSize(40, 40))
         self.start_btn.setObjectName("primary_btn")
         self.start_btn.setMinimumSize(120, 50)
         self.start_btn.setMaximumSize(120, 50)
         self.grouped_btn_layout.addWidget(self.start_btn)
 
-        self.pause_btn = QPushButton("Pause")
+        self.pause_btn = QPushButton("")
+        self.pause_btn.setIcon(QIcon(QPixmap(":svg/pause-fill.svg")))
+        self.pause_btn.setIconSize(QSize(40, 40))
         self.pause_btn.setObjectName("primary_btn")
         self.pause_btn.setMinimumSize(120, 50)
         self.pause_btn.setMaximumSize(120, 50)
 
-        self.stop_btn = QPushButton("Stop")
+        self.stop_btn = QPushButton("")
+        self.stop_btn.setIcon(QIcon(QPixmap(":/svg/stop-fill.svg")))
+        self.stop_btn.setIconSize(QSize(40, 40))
         self.stop_btn.setObjectName("primary_btn")
         self.stop_btn.setMinimumSize(120, 50)
         self.stop_btn.setMaximumSize(120, 50)
 
-        self.resume_btn = QPushButton("Resume")
+        self.resume_btn = QPushButton("")
+        self.resume_btn.setIcon(QIcon(QPixmap(":/svg/play-fill.svg")))
+        self.resume_btn.setIconSize(QSize(40, 40))
         self.resume_btn.setObjectName("primary_btn")
         self.resume_btn.setMinimumSize(120, 50)
         self.resume_btn.setMaximumSize(120, 50)
@@ -105,40 +116,58 @@ class Timer(QWidget):
         self.stop_btn.clicked.connect(self.stop_timer)
         self.resume_btn.clicked.connect(self.resume_timer)
 
-        # timer set up
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update_timer)
-        self.timer.timeout.connect(self.sync_progressbar)
-        self.time_left = QTime(0, 25, 0)
-
         self.circle_path_timer_stylesheet = """#circle_path{
                     background-color: qconicalgradient(cx:0.5, cy:0.5, angle:90, stop:{stop1} rgba(0, 0, 0, 0), stop:{stop2} rgba(255, 255, 255, 255));
                     border-radius: 149px;
                 }"""
 
-        # time complies val
-        self.time_limit = self.time_left.msecsSinceStartOfDay()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_timer)
+        self.timer.timeout.connect(self.sync_progressbar)
+
         self.stop1 = 1
         self.stop2 = 1
-        #self.stop1 = round((100 - self.time_limit) / 10000000)
-        # functionality
+
+    def start_time_routine(self):
+        routines = [
+            (QTime(0, 25, 0), "Pomodoro"),
+            (QTime(0, 5, 0), "Short Break"),
+            (QTime(0, 25, 0), "Pomodoro"),
+            (QTime(0, 5, 0), "Short Break"),
+            (QTime(0, 25, 0), "Pomodoro"),
+            (QTime(0, 5, 0), "Short Break"),
+            (QTime(0, 25, 0), "Pomodoro"),
+            (QTime(0, 5, 0), "Short Break"),
+            (QTime(0, 25, 0), "Pomodoro"),
+            (QTime(0, 20, 0), "Long Break")
+        ]
+
+        for time, label in routines:
+            self.time_left = time
+            self.modus_label.setText(label)
+            self.progressbar.timer_label.setText(time.toString(Qt.TextDate))
+
+            self.time_limit_const = - (1 / self.time_left.secsTo(QTime(0, 0)))
+            self.timer.start(1000)
+
+            if self.time_left == QTime(0, 0) :
+                self.reset_progressbar()
+
 
     def sync_progressbar(self):
-        self.time_limit = self.time_limit - 1000
-        self.stop2 = round(self.stop1 - 0.001, 3)
+        self.stop2 = self.stop1 - self.time_limit_const
+        print(self.stop1, self.stop2)
 
         # change stylesheet ## var name issue + based on time issue
         self.new_circle_path_timer_stylesheet = self.circle_path_timer_stylesheet.replace("{stop1}", str(self.stop2)).replace("{stop2}", str(self.stop1))
 
         self.progressbar.circle_path.setStyleSheet(self.new_circle_path_timer_stylesheet)
 
-        self.stop1 = round(self.stop1 - 0.001, 3)
+        self.stop1 = self.stop1 - self.time_limit_const
 
     def reset_progressbar(self):
         self.stop1 = 1
         self.stop2 = 0.99999
-
-        print(self.time_limit, self.stop1, self.stop2)
 
         self.new_circle_path_timer_stylesheet = self.circle_path_timer_stylesheet.replace("{stop1}",
                                                                                           str(self.stop2)).replace(
@@ -147,16 +176,15 @@ class Timer(QWidget):
         self.stop1 = 1
         self.stop2 = 1
 
-        print(self.time_limit, self.stop1, self.stop2)
-
     def start_timer(self):
-        self.timer.start(1000)
 
         self.grouped_btn_layout.removeWidget(self.start_btn)
         self.start_btn.setParent(None)
 
         self.grouped_btn_layout.addWidget(self.pause_btn)
         self.grouped_btn_layout.addWidget(self.stop_btn)
+
+        self.start_time_routine()
 
     def update_timer(self):
         self.time_left = self.time_left.addSecs(-1)
@@ -180,6 +208,8 @@ class Timer(QWidget):
         self.grouped_btn_layout.addWidget(self.stop_btn)
 
     def resume_timer(self):
+        self.timer.start(1000)
+
         self.grouped_btn_layout.removeWidget(self.resume_btn)
         self.resume_btn.setParent(None)
 
@@ -188,8 +218,6 @@ class Timer(QWidget):
 
         self.grouped_btn_layout.addWidget(self.pause_btn)
         self.grouped_btn_layout.addWidget(self.stop_btn)
-
-        self.timer.start(1000)
 
     def stop_timer(self):
         self.timer.stop()
@@ -207,4 +235,3 @@ class Timer(QWidget):
         self.resume_btn.setParent(None)
 
         self.grouped_btn_layout.addWidget(self.start_btn)
-
